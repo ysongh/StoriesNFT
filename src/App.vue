f8f4192f821b201ac0e0d03bf6b741395babce97
You Song Hou <yousonghou@yahoo.com>
2021-03-19 22:13:54 -0400

    set up the project with vue and truffle

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Stories NFT</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
