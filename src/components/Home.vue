1c29446c1738a490dee23adf8aafe0608c92d413
You Song Hou <yousonghou@yahoo.com>
2021-03-30 18:43:53 -0400

    add a function to upload a file to space storage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -34,7 +34,17 @@
 
     <p class="mt-3">Your files</p>
     <div v-bind:key="file.created" v-for="file of files">
-      <p>{{file.name}}</p>
+      <p @click="getFile()">{{file.name}}</p>
+    </div>
+
+    <div class="form-group">
+      <label class="font-weight-bold">Upload File</label>
+      <div class="input-group">
+        <div class="custom-file">
+          <input type="file" class="custom-file-input" @change="uploadFile">
+          <label class="custom-file-label"></label>
+        </div>
+      </div>
     </div>
   </div>
    
@@ -42,7 +52,7 @@
 
 <script>
 import { Users, BrowserStorage } from '@spacehq/users';
-import { UserStorage } from "@spacehq/storage";
+import { UserStorage, AddItemsResultSummary } from "@spacehq/storage";
 import { mapGetters, mapActions } from 'vuex';
 
 export default {
@@ -76,10 +86,12 @@ export default {
 
       let userList = await storage.list()
       console.log("storage.list(): ", await storage.list());
+      this.userList = userList;
 
       console.log(userList, "fds")
       
       const spaceUser = await users.authenticate(userList[num - 1]);
+      this.spaceUser = spaceUser;
       console.log(spaceUser, "spaceUser")
 
       const spaceStorage = new UserStorage(spaceUser);
@@ -90,6 +102,79 @@ export default {
       console.log(result.items, "result")
 
       this.files = result.items;
+    },
+    async uploadFile(event){
+      const sourcePaths = Array.from(event.target.files).map((file) => ({
+        name: file.name,
+        size: file.size,
+        data: file,
+        mimeType: file.type,
+        path: file.webkitRelativePath || file.name,
+      }));
+      console.log(sourcePaths);
+
+      const spaceStorage = new UserStorage(this.spaceUser);
+
+      const uploadResponse = await spaceStorage.addItems({
+        bucket: 'personal',
+        files: [
+          {
+            data: sourcePaths[0].data,
+            path: sourcePaths[0].path,
+            mimeType: sourcePaths[0].mimeType,
+          }
+        ]
+      });
+
+      console.log(uploadResponse, "uploadResponse")
+      
+      uploadResponse.once('done', async (AddItemsEventData) => {
+        const summary = AddItemsResultSummary;
+        console.log(summary, "summary", AddItemsEventData);
+
+        let result;
+
+        result = await spaceStorage.listDirectory({ bucket: 'personal', path: '/' });
+        console.log(result, "result")
+
+        const response = await spaceStorage.openFile({ bucket: 'personal', path: sourcePaths[0].path });
+        // response.stream is an async iterable
+        for await (const chunk of response.stream) {
+          // aggregate the chunks based on your logic
+          console.log(chunk, "chunk")
+        }
+
+        // response also contains a convenience function consumeStream
+        const fileBytes = await response.consumeStream();
+        console.log(fileBytes, "fileBytes")
+        const fileUrl = URL.createObjectURL(new Blob([fileBytes.buffer], { type: response.mimeType }));
+        console.log(fileUrl, "fileUrl")
+        this.url = fileUrl;
+      });
+    },
+    async getFile(){
+      const spaceStorage = new UserStorage(this.spaceUser);
+
+      const response = await spaceStorage.openFile({ bucket: 'personal', path: '/photo-1414235077428-338989a2e8c0.jpeg' });
+      // response.stream is an async iterable
+      for await (const chunk of response.stream) {
+        // aggregate the chunks based on your logic
+        console.log(chunk, "chunk")
+      }
+
+      function typedArrayToUrl(buffer, mimeType){
+        return URL.createObjectURL(new Blob(buffer, { type: mimeType }));
+      }
+
+      // response also contains a convenience function consumeStream
+      const fileBytes = await response.consumeStream();
+      console.log(fileBytes, "fileBytes")
+      //const fileUrl = URL.createObjectURL(new Blob([fileBytes.buffer], { type: response.mimeType }));
+      const fileUrl = typedArrayToUrl([fileBytes.buffer], response.mimeType);
+      console.log(fileUrl, "fileUrl")
+      // let src = `data:${response.mimeType};base64,` + fileBytes;
+      // console.log(src)
+      this.url = fileUrl;
     }
   },
   computed: mapGetters(['walletAddress', 'storiesBlockchain', 'storiesCount', 'storiesList']),
@@ -107,49 +192,6 @@ export default {
     const users = await Users.withStorage(storage, {endpoint: "wss://auth.space.storage"}, onErrorCallback);
     this.userList = users.list();
     this.usersLen = users.list().length;
-
-    // const storage = new UserStorage(spaceUser);
-    // this.spaceStorage = storage;
-    // console.log(storage, "storage")
-
-    // await storage.createFolder({ bucket: 'personal', path: '/hello' });
-    // let result = await storage.listDirectory({ bucket: 'personal', path: '/' });
-    // console.log(result, "result")
-
-    // const uploadResponse = await storage.addItems({
-    //   bucket: 'personal',
-    //   files: [
-    //     {
-    //       path: '/logo',
-    //       data: '',
-    //       mimeType: 'image/png'
-    //     }
-    //   ],
-    // });
-
-    // console.log(uploadResponse, "uploadResponse")
-    
-    // uploadResponse.once('done', async (AddItemsEventData) => {
-    //   const summary = AddItemsResultSummary;
-    //   console.log(summary, "summary", AddItemsEventData);
-
-    //    result = await storage.listDirectory({ bucket: 'personal', path: '/' });
-    //    console.log(result, "result")
-
-    //   const response = await storage.openFile({ bucket: 'personal', path: '/logo' });
-    //   // response.stream is an async iterable
-    //   for await (const chunk of response.stream) {
-    //     // aggregate the chunks based on your logic
-    //     console.log(chunk, "chunk")
-    //   }
-
-    //   // response also contains a convenience function consumeStream
-    //   const fileBytes = await response.consumeStream();
-    //   console.log(fileBytes, "fileBytes")
-    //   const fileUrl = URL.createObjectURL(new Blob([fileBytes.buffer], { type: response.mimeType }));
-    //   console.log(fileUrl, "fileUrl")
-    //   this.url = fileUrl;
-    // });
   }
 }
 </script>
